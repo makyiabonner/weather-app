0cbe493eef7107ce49fbcf90b1426b49fdcf3a4a
makyia bonner <makyiabonner@gmail.com>
2023-02-09 19:25:05 -0500

    HTML content for news

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 
-document.querySelector('button').addEventListener('click', cityPic)
-
+document.querySelector('button').addEventListener('click', test)
+function test(){
+let city = document.querySelector('#city').value.toLowerCase()
+fetch(`http://api.mediastack.com/v1/news?access_key=e046e8d7db98d4cf3a1bf2f9d5897f8e&keywords=${city}&sources=en,-de`)
+.then(res => res.json())
+.then(data =>{
+    console.log(data);
+})
+}
