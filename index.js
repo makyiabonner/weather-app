25668d672595b69de1375c6bcb8d82649587df87
makyia bonner <makyiabonner@gmail.com>
2023-02-10 17:10:33 -0500

    Prevent headlines from overflow JS

--- index.js
+++ index.js
@@ -24,24 +24,36 @@ fetch(`http://api.mediastack.com/v1/news?access_key=e046e8d7db98d4cf3a1bf2f9d589
     topDesc.textContent = data.data[0].description;
 
     hline1Img.src = data.data[1].image;
-    hline1p.textContent = data.data[1].description;
 
     hline2Img.src = data.data[2].image;
-    hline2p.textContent = data.data[2].description;
 
     //Prevent text overflow for headline and top news
     //Top News
     //Headline 1
+        //Title
         if(data.data[1].title.length >= 60){
             hline1Title.textContent = data.data[1].title.substring(0,60)+'...';
         }
         else hline1Title.textContent = data.data[1].title;
 
+        //Description
+        if(data.data[1].description.length >= 90){
+            hline1p.textContent = data.data[1].description.substring(0,90)+'...';
+        }
+        else hline1p.textContent = data.data[1].description;
+
     //Headline 2
+        //Title
         if(data.data[2].title.length >= 60){
             hline2Title.textContent = data.data[2].title.substr(0,60) +'...';
         }
         else hline2Title.textContent = data.data[2].title;
+        
+        //Description
+        if(data.data[2].description.length >= 90){
+            hline2p.textContent = data.data[2].description.substring(0,90)+'...';
+        }
+        else hline2p.textContent = data.data[2].description;
     
     if(data.data[1].image == undefined || data.data[1].image == null){
         return hline1Img.src = 'https://cdn.vectorstock.com/i/preview-1x/82/99/no-image-available-like-missing-picture-vector-43938299.jpg';
