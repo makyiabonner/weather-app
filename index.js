166b653c9270dbe3172f2f27131e9529f6404762
makyia bonner <makyiabonner@gmail.com>
2023-02-10 16:47:54 -0500

    Prevent overflow of headline textbox

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let topTitle = document.querySelector('.top-title')
 let topImg = document.querySelector('.top-news').style.background;
-let topDesc = document.querySelector('.hline2-title');
+let topDesc = document.querySelector('.top-p');
 
 let hline1Title = document.querySelector('.hline1-title');
 let hline1Img = document.querySelector('.hline1-img');
@@ -10,10 +10,10 @@ let hline2Title = document.querySelector('.hline2-title');
 let hline2Img = document.querySelector('.hline2-img');
 let hline2p = document.querySelector('.hline2-p');
 
-document.querySelector('button').addEventListener('click', test)
+document.querySelector('button').addEventListener('click', news)
 
 
-function test(){
+function news(){
 let city = document.querySelector('#city').value.toLowerCase()
 fetch(`http://api.mediastack.com/v1/news?access_key=e046e8d7db98d4cf3a1bf2f9d5897f8e&keywords=${city}&sources=en,-de`)
 .then(res => res.json())
@@ -23,19 +23,30 @@ fetch(`http://api.mediastack.com/v1/news?access_key=e046e8d7db98d4cf3a1bf2f9d589
     topTitle.textContent = data.data[0].title;
     topDesc.textContent = data.data[0].description;
 
-    hline1Title.textContent = data.data[1].title;
     hline1Img.src = data.data[1].image;
     hline1p.textContent = data.data[1].description;
 
-    hline2Title.textContent = data.data[2].title;
     hline2Img.src = data.data[2].image;
     hline2p.textContent = data.data[2].description;
 
+    //Prevent text overflow for headline and top news
+    if(data.data[1].title.length >= 60){
+        hline1Title.textContent = data.data[1].title.substring(0,60)+'...';
+        }
+    else hline1Title.textContent = data.data[1].title;
+
+    if(data.data[2].title.length >= 60){
+        hline2Title.textContent = data.data[2].title.substr(0,60) +'...';
+    }
+    else hline2Title.textContent = data.data[2].title;
+    
     if(data.data[1].image == undefined || data.data[1].image == null){
         return hline1Img.src = 'https://cdn.vectorstock.com/i/preview-1x/82/99/no-image-available-like-missing-picture-vector-43938299.jpg';
     }
     if(data.data[2].image == undefined || data.data[2].image == null){
         return hline2Img.src = 'https://cdn.vectorstock.com/i/preview-1x/82/99/no-image-available-like-missing-picture-vector-43938299.jpg';
+        
     }
 })
+
 }
