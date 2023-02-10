b413ed14a41426703b2daef9c43c54d8c85cc237
makyia bonner <makyiabonner@gmail.com>
2023-02-09 20:04:56 -0500

    Modified HTML News classes

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 let topTitle = document.querySelector('.top-title').textContent
-let topImg = document.querySelector('.headline1-title').textContent
-let topDesc = document.querySelector('.headline2-title').textContent
+let topImg = document.querySelector('.top-news').style.background;
+let topDesc = document.querySelector('.hline2-title').textContent
+
+let hline1Title = document.querySelector('.hline1-title').textContent
+let hline1Img = document.querySelector('.hline1-img').src
+let hline1p = document.querySelector('.hline1-p').textContent
+
+let hline2Title = document.querySelector('.hline2-title').textContent
+let hline2Img = document.querySelector('.hline2-img').src
+let hline2p = document.querySelector('.hline2-p').textContent
+
 document.querySelector('button').addEventListener('click', test)
 function test(){
 let city = document.querySelector('#city').value.toLowerCase()
