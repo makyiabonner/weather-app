6ab2b458b9e0ee2a05c8b67210f210750925ad09
makyia bonner <makyiabonner@gmail.com>
2023-02-11 16:20:21 -0500

    Changed API because lack of HTTPS support

--- index.js
+++ index.js
@@ -16,51 +16,55 @@ document.querySelector('button').addEventListener('click', news)
 function news(){
 let city = document.querySelector('#city').value.toLowerCase()
 //My subscription doesn't support https
-fetch(`http://api.mediastack.com/v1/news?access_key=e046e8d7db98d4cf3a1bf2f9d5897f8e&keywords=${city}&sources=en,-de`, {referrerPolicy: "unsafe_url"})
+
+fetch(`https://newsapi.org/v2/everything?q=${city}&apiKey=dff81faf29a9453b9309efa6b5b274df`)
 .then(res => res.json())
 .then(data =>{
     console.log(data)
 
-    topTitle.textContent = data.data[0].title;
-    topDesc.textContent = data.data[0].description;
+    topTitle.textContent = data.articles[0].title;
+    topDesc.textContent = data.articles[0].description;
 
-    hline1Img.src = data.data[1].image;
+    hline1Img.src = data.articles[1].urlToImage;
 
-    hline2Img.src = data.data[2].image;
+    hline2Img.src = data.articles[2].urlToImage;
 
-    //Prevent text overflow for headline and top news
-    //Top News
-    //Headline 1
-        //Title
-        if(data.data[1].title.length >= 60){
-            hline1Title.textContent = data.data[1].title.substring(0,60)+'...';
-        }
-        else hline1Title.textContent = data.data[1].title;
-
-        //Description
-        if(data.data[1].description.length >= 90){
-            hline1p.textContent = data.data[1].description.substring(0,90)+'...';
-        }
-        else hline1p.textContent = data.data[1].description;
-
-    //Headline 2
-        //Title
-        if(data.data[2].title.length >= 60){
-            hline2Title.textContent = data.data[2].title.substr(0,60) +'...';
-        }
-        else hline2Title.textContent = data.data[2].title;
-        
-        //Description
-        if(data.data[2].description.length >= 90){
-            hline2p.textContent = data.data[2].description.substring(0,90)+'...';
-        }
-        else hline2p.textContent = data.data[2].description;
+//Prevent text overflow for headline and top news
+  //Top News
+      //Title
+      //Description
     
-    if(data.data[1].image == undefined || data.data[1].image == null){
-        return hline1Img.src = 'https://cdn.vectorstock.com/i/preview-1x/82/99/no-image-available-like-missing-picture-vector-43938299.jpg';
+  //Headline 1
+      //Title
+        if(data.articles[1].title.length >= 60){
+            hline1Title.textContent = data.articles[1].title.substring(0,60)+'...';
+        }
+        else hline1Title.textContent = data.articles[1].title;
+
+      //Description
+        if(data.articles[1].description.length >= 90){
+            hline1p.textContent = data.articles[1].description.substring(0,90)+'...';
+        }
+        else hline1p.textContent = data.articles[1].description;
+
+  //Headline 2
+      //Title
+        if(data.articles[2].title.length >= 60){
+            hline2Title.textContent = data.articles[2].title.substr(0,60) +'...';
+        }
+        else hline2Title.textContent = data.articles[2].title;
+        
+      //Description
+        if(data.articles[2].description.length >= 90){
+            hline2p.textContent = data.articles[2].description.substring(0,90)+'...';
+        }
+        else hline2p.textContent = data.articles[2].description;
+    
+    if(data.articles[1].urlToImage == undefined || data.articles[1].urlToImage == null){
+        return hline1Img.src = 'https://cdn.vectorstock.com/i/preview-1x/82/99/no.urlToImage-available-like-missing-picture-vector-43938299.jpg';
     }
-    if(data.data[2].image == undefined || data.data[2].image == null){
-        return hline2Img.src = 'https://cdn.vectorstock.com/i/preview-1x/82/99/no-image-available-like-missing-picture-vector-43938299.jpg';
+    if(data.articles[2].urlToImage == undefined || data.articles[2].urlToImage == null){
+        return hline2Img.src = 'https://cdn.vectorstock.com/i/preview-1x/82/99/no.urlToImage-available-like-missing-picture-vector-43938299.jpg';
         
     }
 })
