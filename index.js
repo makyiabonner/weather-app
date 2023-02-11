cbddcc32d156530bf0f7a681f4ebcb798b55a76c
makyia bonner <makyiabonner@gmail.com>
2023-02-11 15:08:23 -0500

    Hotfix api HTTP to HTTPS

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ document.querySelector('button').addEventListener('click', news)
 
 function news(){
 let city = document.querySelector('#city').value.toLowerCase()
-fetch(`http://api.mediastack.com/v1/news?access_key=e046e8d7db98d4cf3a1bf2f9d5897f8e&keywords=${city}&sources=en,-de`)
+fetch(`https://api.mediastack.com/v1/news?access_key=e046e8d7db98d4cf3a1bf2f9d5897f8e&keywords=${city}&sources=en,-de`)
 .then(res => res.json())
 .then(data =>{
     console.log(data)
