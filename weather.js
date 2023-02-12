4977ef33fc855147d27c09eab5fe484ef490e96b
makyia bonner <makyiabonner@gmail.com>
2023-02-11 22:24:49 -0500

    Weather top and main content from API to DOM

--- weather.js
+++ weather.js
@@ -1,5 +1,5 @@
 let cName = document.querySelector('.city-name');
-let date = document.querySelector('.day and date');
+let date = document.querySelector('.day-and-date');
 let clock = document.querySelector('.time');
 let farenheit = document.querySelector('.main-left-farenheit');
 let tempDesc = document.querySelector('.main-left-temp-desc');
@@ -18,6 +18,16 @@ function weather(){
     .then(res => res.json())
     .then(data =>{
         console.log(data)
-        cName.textContent = data.location.name + ',' + data.location.region
+        cName.textContent = data.location.name + ',' + data.location.region;
+        date.textContent = data.forecast.forecastday[0].date.toString();
+        farenheit.textContent = data.current.temp_f.toString();
+        tempDesc.textContent = data.forecast.forecastday[0].day.condition.text.toString();
+        highTemp.textContent = data.forecast.forecastday[0].day.maxtemp_f.toString();
+        mph.textContent = data.current.wind_mph.toString();
+        riseTime.textContent = data.forecast.forecastday[0].astro.sunrise;
+        lowTemp.textContent = data.forecast.forecastday[0].day.mintemp_f.toString();
+        rainPer.textContent = data.forecast.forecastday[0].day.daily_chance_of_rain.toString()
+        setTime.textContent = data.forecast.forecastday[0].astro.sunset
+
     })
 }
